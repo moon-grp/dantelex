fba9ecb8c178dc66e9551f5663bf08b29bee4b6c
moon-grp <olumidemm@gmail.com>
2020-07-08 19:58:39 +0100

    starting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,131 @@
+/* eslint-disable prettier/prettier */ /* eslint-disable prettier/prettier */
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="cal">
+    <div class="dis ">{{ display || 0 }}</div>
+    <div class="btns num">history</div>
+    <div class="btns num"><i class="fas fa-ruler-horizontal"></i></div>
+    <div class="btns num">symbol</div>
+    <div class="btns num" @click="clear"><i class="fas fa-backspace"></i></div>
+    <div class="btns num" style="color:red" @click="clear">C</div>
+    <div class="op btns num">`()`</div>
+    <div class="op btns num" @click="percentage">%</div>
+    <div class="op btns num" @click="divide">/</div>
+    <div class="btns num" @click="pick('7')">7</div>
+    <div class="btns num" @click="pick('8')">8</div>
+    <div class="btns" @click="pick('9')">9</div>
+    <div class="op btns num" @click="mult">x</div>
+    <div class="btns num" @click="pick('4')">4</div>
+    <div class="btns num" @click="pick('5')">5</div>
+    <div class="btns num" @click="pick('6')">6</div>
+    <div class="op btns num" @click="minus">-</div>
+    <div class="btns num" @click="pick('1')">1</div>
+    <div class="btns num" @click="pick('2')">2</div>
+    <div class="btns num" @click="pick('3')">3</div>
+    <div class="op btns num" @click="add">+</div>
+    <div class="btns num" @click="negative">+/-</div>
+    <div class="btns num" @click="pick('0')">0</div>
+    <div class="btns num" @click="dotb">.</div>
+    <div class="eq num" @click="equalsto">=</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      display: "",
+      ops: null,
+      previous: null,
+      opsclick: false,
+    };
+  },
+  methods: {
+    setp() {
+      this.previous = this.display;
+      this.opsclick = true;
+    },
+    clear() {
+      this.display = "";
+    },
+    negative() {
+      this.display =
+        this.display.charAt(0) === "-"
+          ? this.display.slice(1)
+          : `-${this.display}`;
+    },
+    percentage() {
+      this.display = `${parseFloat(this.display) / 100}`;
+    },
+    pick(num) {
+      if (this.opsclick) {
+        this.display = "";
+        this.opsclick = false;
+      }
+      this.display = `${this.display}${num}`;
+    },
+    dotb() {
+      if (this.display.indexOf(".") === -1) {
+        this.pick(".");
+      }
+    },
+    mult() {
+      this.ops = (a, b) => a * b;
+      this.setp();
+    },
+    divide() {
+      this.ops = (a, b) => a / b;
+      this.setp();
+    },
+    minus() {
+      this.ops = (a, b) => a - b;
+      this.setp();
+    },
+    add() {
+      this.ops = (a, b) => a + b;
+      this.setp();
+    },
+    equalsto() {
+      this.display = `${this.ops(
+        parseFloat(this.display),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.cal {
+  width: 350px;
+  margin: 0 auto;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  display: grid;
+  font-size: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.dis {
+  grid-column: 1 / 5;
+  grid-row: 1/6;
+  margin-top: 5px;
+  background-color: red;
+  padding-top: 50%;
+  padding-left: 80%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.num {
+  padding-top: 10px;
 }
-a {
-  color: #42b983;
+.eq {
+  background: skyblue;
+  color: white;
+}
+.op {
+  color: skyblue;
+}
+.btns {
+  background-color: ghostwhite;
+  border: 0.5px solid #333;
 }
 </style>
